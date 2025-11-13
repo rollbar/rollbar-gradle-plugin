5fc0379e0ca7c26c0a97f5051b23742bd36feaf2
chris <christian.buongarzoni@gmail.com>
2025-11-13 02:28:16 -0300

    feat: add gradle publish plugin and plugin info

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `kotlin-dsl`
     `maven-publish`
+    id("com.gradle.plugin-publish") version "2.0.0"
 }
 
 group = properties["GROUP"].toString()
@@ -14,8 +15,14 @@ dependencies {
 
 gradlePlugin {
     plugins {
+        website = "https://rollbar.com/"
+        vcsUrl = "https://github.com/rollbar/rollbar-gradle-plugin"
+
         create("rollbar-android") {
             id = properties["PLUGIN_NAME"].toString()
+            displayName = "Rollbar gradle plugin"
+            description = "This Plugin uploads your mapping files to Rollbar"
+            tags = listOf("upload", "mappings", "android", "Rollbar")
             implementationClass = properties["PLUGIN_NAME_CLASS"].toString()
         }
     }
